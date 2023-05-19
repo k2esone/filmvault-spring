5f4920bb666b102829239163331592b34add6e46
dwydm <dwydmuch@gmail.com>
2023-05-19 18:20:10 +0200

    first commit

---
+++ src/main/java/pl/ccteamone/filmvault/FilmVaultApplication.java
@@ -0,0 +1,13 @@
+package pl.ccteamone.filmvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmVaultApplication.class, args);
+    }
+
+}
