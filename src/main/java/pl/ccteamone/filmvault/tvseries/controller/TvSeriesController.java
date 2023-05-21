ae2ae9c46c376caf4fecfd5fb110d4d6abf90311
dwydm <dwydmuch@gmail.com>
2023-05-21 18:02:04 +0200

    TSE-1/TvSeries basic entity configuration

--- src/main/java/pl/ccteamone/filmvault/tvseries/controller/TvSeriesController.java
+++ src/main/java/pl/ccteamone/filmvault/tvseries/controller/TvSeriesController.java
@@ -1,4 +1,33 @@
 package pl.ccteamone.filmvault.tvseries.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.ccteamone.filmvault.tvseries.dto.TvSeriesDto;
+import pl.ccteamone.filmvault.tvseries.service.TvSeriesService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/tvseries")
 public class TvSeriesController {
+
+    private final TvSeriesService tvService;
+
+    @GetMapping("/list")
+    public List<TvSeriesDto> getTvSeriesList() {
+        return tvService.getTvSeriesDtoList();
+    }
+
+    @GetMapping("/{id}")
+    public TvSeriesDto getTvSeries(@PathVariable("id") UUID id) {
+        return tvService.getTvSeriesDtoById(id);
+    }
+
+
+
 }
---
+++ src/main/java/pl/ccteamone/filmvault/tvseries/repository/TvSeriesRepository.java
@@ -0,0 +1,11 @@
+package pl.ccteamone.filmvault.tvseries.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.ccteamone.filmvault.tvseries.TvSeries;
+import java.util.UUID;
+
+public interface TvSeriesRepository extends CrudRepository<TvSeries, UUID> {
+
+
+}
