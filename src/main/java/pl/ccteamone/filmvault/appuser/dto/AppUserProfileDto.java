7e1a465c2c9038ec6da59e8c0cf312e08b6e4cc8
k2esone <dkowalczyk.lwc@gmail.com>
2023-06-25 08:08:15 +0200

    AppProfileDTO fix, add builder and data

--- src/main/java/pl/ccteamone/filmvault/appuser/dto/AppUserProfileDto.java
+++ src/main/java/pl/ccteamone/filmvault/appuser/dto/AppUserProfileDto.java
@@ -1,10 +1,14 @@
 package pl.ccteamone.filmvault.appuser.dto;
 
+import lombok.Builder;
+import lombok.Data;
 import pl.ccteamone.filmvault.movie.dto.MovieDto;
 import pl.ccteamone.filmvault.tvseries.dto.TvSeriesDto;
 
 import java.util.Set;
 
+@Data
+@Builder
 public class AppUserProfileDto {
     private Long id;
     private String email;
